083bfbb34db02d2cc68756aa2632126558a2303a
Chucky2401 <chuckydu24@gmail.com>
2023-07-29 15:38:57 +0200

    feat: backup vscode server config and PS scripts to copy files

---
+++ scripts/Copy-ToWsl.ps1
@@ -0,0 +1,3 @@
+$rootPath = (Get-Item $PSScriptRoot).parent.FullName
+
+Copy-Item -Path "$($rootPath)\*" -Exclude "LICENSE", "README.md", "scripts", ".git" -Recurse -Destination "\\wsl.localhost\Alpine\srv\docker\build\minecraft\" -Force -PassThru
