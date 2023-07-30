552643fc843db5754eb08178f638b83a22873801
Chucky2401 <chuckydu24@gmail.com>
2023-07-31 00:12:25 +0200

    feat(Entrypoint): add export PATH (does not work). Add java extra args. When container shutdown send /stop to MC server to clean shutdown

--- entrypoint/docker-entrypoint.sh
+++ entrypoint/docker-entrypoint.sh
@@ -1,22 +1,33 @@
-#!/bin/ash
+#!/bin/sh
+
+trap shutdown 1 2 3 6 9 14 15 20
 
 set -Eeuo pipefail
+pythonArg=$1
+shift
 
 function shutdown()
 {
     stop-server
+    wait "$javaPid"
 }
 
-pythonArg=$1
-shift
+export PATH=$PATH:/usr/local/bin/mcserver
 
-#python3 download-minecraft.py $pythonArg
-
-#echo "Install result: $?"
+cd "$(dirname "$0")"
+python3 download-minecraft.py $pythonArg
+echo "Install result: $?"
 
 cd /mcserver
-exec java -Xms1G -Xmx1G -jar server.jar nogui
+javaExtraArgs="--add-modules=jdk.incubator.vector -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20"
+exec java -Xms1G -Xmx1G $javaExtraArgs -jar server.jar --nogui &
+javaPid=`pgrep -f java | xargs`
 
-trap shutdown 1 2 3 6 9 14 15 20
+sleep 30s
 
-exec "$@"
+echo "[hit enter key to exit] or run 'docker stop <container>'"
+read
+
+shutdown
+
+echo "Server shutdown!"
