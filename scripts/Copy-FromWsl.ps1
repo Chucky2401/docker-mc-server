78138cedc182adf2de04e3d28bd8f782ca908255
Chucky2401 <chuckydu24@gmail.com>
2023-09-11 22:34:20 +0200

    chore: remove folders ignore

--- scripts/Copy-FromWsl.ps1
+++
@@ -1,5 +0,0 @@
-$rootPath = (Get-Item $PSScriptRoot).parent.FullName
-
-Copy-Item -Path "\\wsl.localhost\Alpine\srv\docker\.vscode" -Recurse -Destination "$($rootPath)\wsl\srv\docker\" -Force -PassThru
-Copy-Item -Path "\\wsl.localhost\Alpine\home\blackwizard\.vscode-server\data\Machine\*" -Recurse -Destination "$($rootPath)\wsl\home\~\.vscode-server\data\Machine\" -Force -PassThru
-Copy-Item -Path "\\wsl.localhost\Alpine\srv\docker\build\minecraft\*" -Recurse -Destination "$($rootPath)\" -Force -PassThru
--- scripts/Copy-ToWsl.ps1
+++
@@ -1,3 +0,0 @@
-$rootPath = (Get-Item $PSScriptRoot).parent.FullName
-
-Copy-Item -Path "$($rootPath)\*" -Exclude "LICENSE", "README.md", "scripts", ".git", "wsl" -Recurse -Destination "\\wsl.localhost\Alpine\srv\docker\build\minecraft\" -Force -PassThru
